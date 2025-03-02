3ac3f3528243b606de6624908edc9ac083107802
Hamia974 <hamiakhalil287@gmail.com>
2025-03-02 15:20:20 +0500

    Update stack reverse.cpp

--- stack/stack reverse.cpp
+++ stack/stack reverse.cpp
@@ -1,14 +1,50 @@
+#include <iostream>
+#include <string>
+using namespace std;
+template<typename T>
+class Stack{
+protected:
+	T* stack;
+	int currentsize;
+	int maxsize;
+public:
+	//default constructor
+	Stack(){
+	}
+	//parameterized constructor
+	Stack(int s){
+		currentsize = 0;
+		maxsize = s;
+		stack = new T[s];
+	}
+	void push(T value){
+		stack[currentsize] = value;
+		currentsize++;
+	}
+	T pop(){
+		return stack[--currentsize];
+
+	}
+	T top(){
+		return stack[currentsize - 1];
+	}
+	bool isempty(){
+		return currentsize == 0;
+	}
+	bool isFull(){
+		return currentsize == maxsize;
+	}
+	void display() {
+		cout << "Values in stack: ";
+		for (int i = 0; i < currentsize; i++) {
+			cout << stack[i] << " ";
+		}
+		cout << endl;
+	}
+};
 void reversed(Stack<string> &s, Stack<string> &reverse){
-	Stack<string> s1(3);
-	Stack<string> s2(3);
 	while (!s.isempty()){
-		s1.push(s.pop());
-	}
-	while (!s1.isempty()){
-		s2.push(s1.pop());
-	}
-	while (!s2.isempty()){
-		reverse.push(s2.pop());
+		reverse.push(s.pop());
 	}
 
 }
